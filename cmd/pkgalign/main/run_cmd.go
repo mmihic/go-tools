5caac9a7793aa26f518fa044f448316cbf5505f0
Matt Mihic <mmihic@chronosphere.io>
2020-07-04 07:14:32 -0400

    Restrict parallelism to prevent fd oom

--- cmd/pkgalign/main/run_cmd.go
+++ cmd/pkgalign/main/run_cmd.go
@@ -12,14 +12,16 @@ import (
 	"go.uber.org/multierr"
 	"gopkg.in/yaml.v2"
 
+	"github.com/mmihic/go-tools/pkg/astio"
 	"github.com/mmihic/go-tools/pkg/path"
-	"github.com/mmihic/go-tools/tools/pkgalign"
+	"github.com/mmihic/go-tools/pkg/pkgs"
 )
 
 type runCmd struct {
 	File         string `short:"f" required:"" help:"name of the configuration file"`
 	LocalPkgRoot string `short:"r" required:"" help:"the local package root"`
 	Dir          string `arg:"" required:"" help:"the directory to start from"`
+	MaxParallel  int    `arg:"" default:"10" help:"max parallelism"`
 }
 
 // Run runs the rewrite tool
@@ -30,7 +32,7 @@ func (cmd *runCmd) Run() error {
 	}
 
 	type config struct {
-		Rules pkgalign.RewriteRules `yaml:"packages"`
+		PkgMoves pkgs.Moves `yaml:"packages"`
 	}
 
 	var cfg config
@@ -38,47 +40,80 @@ func (cmd *runCmd) Run() error {
 		return fmt.Errorf("unable to parse config: %v", err)
 	}
 
-	rules := cfg.Rules.ApplyPrefix(path.NewPath(cmd.LocalPkgRoot))
+	rules := cfg.PkgMoves.ApplyPrefix(path.NewPath(cmd.LocalPkgRoot))
 
-	var dirs []string
+	var (
+		wg      sync.WaitGroup
+		errorCh = make(chan error, 1000)
+		dirsCh  = make(chan string, 1000)
+		allDone = make(chan struct{})
+		allErr  error
+	)
+
+	// Process directories in parallel
+	for i := 0; i < cmd.MaxParallel; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+
+			for dir := range dirsCh {
+				if err := cmd.processDir(dir, rules); err != nil {
+					errorCh <- err
+				}
+			}
+		}()
+	}
+
+	// Combine errors
+	go func() {
+		for err := range errorCh {
+			allErr = multierr.Append(allErr, err)
+		}
+		close(allDone)
+	}()
+
+	// Feed in all the directories
 	if err := filepath.Walk(cmd.Dir, func(path string, info os.FileInfo, err error) error {
 		if info.IsDir() {
-			dirs = append(dirs, path)
+			dirsCh <- path
 		}
 		return nil
 	}); err != nil {
 		return err
 	}
+	close(dirsCh)
 
-	var (
-		wg      sync.WaitGroup
-		errorCh = make(chan error, len(dirs))
-	)
-	for _, dir := range dirs {
-		dir := dir
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-
-			fset := token.NewFileSet()
-			pkgs, err := parser.ParseDir(fset, dir, nil, parser.ParseComments)
-			if err != nil {
-				errorCh <- fmt.Errorf("could not parse %s: %v", dir, err)
-				return
-			}
-
-			for _, pkg := range pkgs {
-				pkgPath := path.NewPath(filepath.Join(cmd.LocalPkgRoot, dir))
-				pkgalign.Rewrite(fset, pkgPath, pkg, rules)
-			}
-		}()
-	}
-
+	// Wait for everything to complete
 	wg.Wait()
 	close(errorCh)
-	var allErr error
-	for err := range errorCh {
-		allErr = multierr.Append(allErr, err)
-	}
+	<-allDone
 	return allErr
+}
+
+func (cmd *runCmd) processDir(dir string, moves pkgs.Moves) error {
+	fset := token.NewFileSet()
+	pkgPath := path.NewPath(filepath.Join(cmd.LocalPkgRoot, dir))
+	packages, err := parser.ParseDir(fset, dir, nil, parser.ParseComments)
+	if err != nil {
+		return fmt.Errorf("could not parse %s: %v", dir, err)
+	}
+
+	for _, pkg := range packages {
+		for _, file := range pkg.Files {
+			fname := fset.File(file.Pos())
+			fmt.Printf("processing %s\n", fname.Name())
+			changed, err := moves.Apply(fset, pkgPath, file)
+			if err != nil {
+				return fmt.Errorf("error applying moves to %s: %v", fname.Name(), err)
+			}
+
+			if changed {
+				if err := astio.WriteFile(fset, file); err != nil {
+					return fmt.Errorf("error applying moves to %s: %v", fname.Name(), err)
+				}
+			}
+		}
+	}
+
+	return nil
 }
